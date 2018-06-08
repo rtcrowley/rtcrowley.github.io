f22184a080860e3441ccc6a1d02f79df7088da31
rtcrowley <crowley.rtc@gmail.com>
2018-06-07 18:56:01 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var vid = document.getElementById("bgvid");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+vid.pause();
+vidFade();
+}); 
